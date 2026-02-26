77e35a1a1c1de164348cd73aab73c66b8ab59a65
HoangThong26 <hbt2k4@gmail.com>
2026-02-26 19:01:16 +0700

    Validate

--- CapstoneProject/CapstoneProject.API/Program.cs
+++ CapstoneProject/CapstoneProject.API/Program.cs
@@ -1,14 +1,24 @@
 using CapstoneProject.Application.Interface.IRepository;
 using CapstoneProject.Application.Interface.IService;
+using CapstoneProject.Infrastructure.Database.AppDbContext;
 using CapstoneProject.Infrastructure.Repostitory;
 using CapstoneProject.Infrastructure.Services;
-using CapstoneProject.Infrastructure.Database.AppDbContext;
-using Microsoft.EntityFrameworkCore;
+using CapstoneProject.Infrastructure.Validation;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<LoginRequestValidator>();
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
